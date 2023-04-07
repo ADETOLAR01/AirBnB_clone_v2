b5d4143075d65733c6436e70b8056b0e8db33c4b
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2023-04-07 06:27:42 +0100

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,22 +1,29 @@
-#!/usr/bin/python3
-# Fabfile to generates a .tgz archive from the contents of web_static.
-import os.path
+from fabric.api import *
 from datetime import datetime
-from fabric.api import local
 
+env.hosts = ['<54.210.83.3>']
+env.user = '<156689-web-01>'
+env.key_filename = '<~/.ssh/your_ssh_key_file>'
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    """Generates a .tgz archive from the contents of the web_static folder"""
+    try:
+        # Create the versions directory if it doesn't exist
+        local("mkdir -p versions")
+
+        # Get the current time and date
+        now = datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+
+        # Create the name of the archive
+        archive_name = "web_static_" + timestamp + ".tgz"
+
+        # Compress the web_static folder into a tgz archive
+        local("tar -cvzf versions/{} web_static".format(archive_name))
+
+        # Return the archive path if it has been correctly generated
+        return "versions/{}".format(archive_name)
+
+    except:
+        # Return None if there was an error generating the archive
         return None
-    return file
