5ec274a90010fb48fc6528a139f387edd5a540cf
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2024-07-19 18:51:29 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,8 +4,8 @@ Fabric script based on the file 1-pack_web_static.py that distributes an
 archive to the web servers
 """
 
-from fabric.api import env, put, run, local
-from os.path import exists
+from fabric.api import env, put, run
+from os
 env.hosts = ['100.26.239.74', '54.89.96.221']
 
 
@@ -16,18 +16,24 @@ def do_deploy(archive_path):
     try:
         # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, '/tmp/')
-        # Get the file name without extension
-        file_name = archive_path.split('/')[1].split('.')[0]
-        # Create the directory where the archive will be uncompressed
-        run('mkdir -p /data/web_static/releases/{}'.format(file_name))
-        # Uncompress the archive to the folder on the web server
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'.format(file_name, file_name))
+
+        # Extract the archive filename without extension
+        archive_filename = os.path.basename(archive_path)
+        archive_folder = archive_filename.split('.')[0]
+
+        # Uncompress the archive to the folder
+        run(f'mkdir -p /data/web_static/releases/{archive_folder}')
+        run(f'tar -xzf /tmp/{archive_filename} -C /data/web_static/releases/{archive_folder}')
+        
         # Delete the archive from the web server
-        run('rm /tmp/{}.tgz'.format(file_name))
+        run(f'rm /tmp/{archive_filename}')
+
         # Delete the symbolic link /data/web_static/current from the web server
         run('rm -rf /data/web_static/current')
-        # Create a new the symbolic link /data/web_static/current on the web server
-        run('ln -s /data/web_static/releases/{} /data/web_static/current'.format(file_name))
+
+        # Create a new symbolic link
+        run(f'ln -s /data/web_static/releases/{archive_folder} /data/web_static/current')
+
         return True
     except:
         return False
