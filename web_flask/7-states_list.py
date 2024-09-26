fe4f235120840d8a9d17aac1941cb5cd720fc86c
ADENIYI <111161031+ADETOLAR01@users.noreply.github.com>
2024-09-26 05:43:15 -0500

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,62 @@
 #!/usr/bin/python3
-"""A simple Flask web application to list states from storage"""
+"""
+starts a Flask web application
+"""
 
-from flask import Flask, render_template
-from models import storage
-from models.state import State
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
-# Route to display list of states
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_is_fun(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route("/python", strict_slashes=False)
+def python():
+    return "Python is cool"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def Python_is_magic(text):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def n_is_a_number(n):
+    return "{:d} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', num=n)
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', num=n)
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays a list of states sorted by name"""
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('states_list.html', states=sorted_states)
+    return render_template('7-states_list.html',
+                           states=storage.all("State"))
 
-# Method to remove SQLAlchemy session after each request
+
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage on teardown"""
+def teardown(err):
     storage.close()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
